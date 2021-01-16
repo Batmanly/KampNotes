e9f61961515a9dd7309388a1bbc5ac4c2e154490
Batmanly <yoldas.recep.288@gmail.com>
2021-01-16 19:51:45 +0300

    Add project files.

---
+++ CsharpGiris/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CsharpGiris
+{
+    class Program
+    {
+        static void Main(string[] args)//Main icindeikler calismaya baslar ilk basta
+        {
+            //string deger tutar char dizisi
+            //type safety - tip guvenli (c# ve java katidir)
+            //Do not repeat yourself - Kendini Tekrarlama (Clean code rule)
+            //deger tutucu alias
+            string kategoeriEtiketi = "Kategori";
+
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+
+            double dolarDun = 7.34;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalis butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artis butonu");
+            }
+            else
+            {
+                Console.WriteLine("Degismedi butonu");
+            }
+
+            if (sistemeGirisYapmisMi == false)
+            {
+                Console.WriteLine("Kullanici Ayaralari");
+
+            }
+            else if (true)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Giris yap butonu");
+            }
+            Console.WriteLine(kategoeriEtiketi);
+
+
+        }
+    }
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(i);
+
+                string kurs1 = " Yazilim gelistirici yetistirme kampi";
+                string kurs2 = " Porgramlama balsnagic icin temel kurs";
+                string kurs3 = "java";
+
+
+            }
+
+        }
+
+    }
+}
